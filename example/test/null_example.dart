7fd02ea388fcfbfc80a71b3013cdc0313a873e54
XinLei <xinlei966@gmail.com>
2021-11-13 23:37:37 +0800

    feat: add late usecase

---
+++ example/test/null_example.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  int a;
+  int b = 1;
+  // sum(a, b);
+}
+
+// 成员变量不能为不赋值
+class A {
+  int x;
+  A({this.x});
+}
+
+// 方法内空检查
+
+// 方法 访问了一个外部的值
+
+// late Key word
+
+// !operator
+
+//
